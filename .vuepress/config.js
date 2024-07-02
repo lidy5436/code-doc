906d60b0ca5cdfa5c4ab5e70674cc8eee99632f4
李东阳 <731729142@qq.com>
2024-07-03 00:01:15 +0800

    【新增】项目初始化

---
+++ .vuepress/config.js
@@ -0,0 +1,32 @@
+const days = require('dayjs')
+const navbar = require('./navbar.js');
+const sidebar = require('./sidebar.js');
+
+module.exports = {
+    title: '李东阳的笔记',
+    description: '李东阳的知识分享和笔记记录平台',
+    head: [
+        ['link', {rel: 'icon', href: '/favicon.ico'}],
+    ],
+    port: 1998,
+    plugins: [
+        ['vuepress-plugin-helper-live2d'],
+        ['vuepress-plugin-zooming'],
+        ['@xiaopanda/vuepress-plugin-code-copy'],
+        ['@vuepress/last-updated',
+            {
+                transformer: (timestamp, lang) => {
+                    return days(timestamp).format('YYYY-MM-DD HH:mm')
+                }
+            }
+        ]
+    ],
+    themeConfig: {
+        logo: '/favicon.ico',
+        repo: 'https://github.com/lidy5436',
+        nav: navbar,
+        sidebar: sidebar,
+        sidebarDepth: 0
+
+    }
+}
