339d08264f5ac2f25d9a71ef4f1ad7e40f505f54
李东阳 <731729142@qq.com>
2024-07-16 22:49:54 +0800

    【新增】部署脚本

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,6 +6,7 @@ const footer = require('./footer');
 module.exports = {
     title: '李东阳的笔记',
     description: '李东阳的知识分享和笔记记录平台',
+    base: '/code-doc/',
     head: [
         ['link', {rel: 'icon', href: '/favicon.ico'}],
     ],
